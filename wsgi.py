502964e0ef433c8dad781d8a4003787cbbd970e7
Romeli9 <119035936+Romeli9@users.noreply.github.com>
2023-04-25 16:13:58 +0700

    Create wsgi.py

---
+++ wsgi.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run()
+
